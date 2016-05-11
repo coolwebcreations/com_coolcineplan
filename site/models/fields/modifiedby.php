b048b54d2d95f3f9ac57861775ff1246370212db
Mike Brandner <mike.brandner@homepagewerkstatt.de>
2016-05-11 15:06:16 +0200

    Add files via upload

---
+++ site/models/fields/modifiedby.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @version    CVS: 0.1.16
+ * @package    Com_Coolcineplan
+ * @author     Mike Brandner <mike@coolwebcreations.de>
+ * @copyright  Copyright (C) coolwebcreations.de 2016. All rights reserved
+ * @license    GNU General Public License Version 2 oder später; siehe LICENSE.txt
+ */
+
+defined('JPATH_BASE') or die;
+
+jimport('joomla.form.formfield');
+
+/**
+ * Supports an HTML select list of categories
+ *
+ * @since  1.6
+ */
+class JFormFieldModifiedby extends JFormField
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var        string
+	 * @since    1.6
+	 */
+	protected $type = 'modifiedby';
+
+	/**
+	 * Method to get the field input markup.
+	 *
+	 * @return   string  The field input markup.
+	 *
+	 * @since    1.6
+	 */
+	protected function getInput()
+	{
+		// Initialize variables.
+		$html   = array();
+		$user   = JFactory::getUser();
+		$html[] = '<input type="hidden" name="' . $this->name . '" value="' . $user->id . '" />';
+		$html[] = "<div>" . $user->name . " (" . $user->username . ")</div>";
+
+		return implode($html);
+	}
+}
---
+++ site/models/fields/submit.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @version    CVS: 0.1.16
+ * @package    Com_Coolcineplan
+ * @author     Mike Brandner <mike@coolwebcreations.de>
+ * @copyright  Copyright (C) coolwebcreations.de 2016. All rights reserved
+ * @license    GNU General Public License Version 2 oder später; siehe LICENSE.txt
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+jimport('joomla.form.formfield');
+
+/**
+ * Class JFormFieldSubmit
+ *
+ * @since  1.6
+ */
+class JFormFieldSubmit extends JFormField
+{
+	protected $type = 'submit';
+
+	protected $value;
+
+	protected $for;
+
+	/**
+	 * Get a form field markup for the input
+	 *
+	 * @return string
+	 */
+	public function getInput()
+	{
+		$this->value = $this->getAttribute('value');
+
+		return '<button id="' . $this->id . '"'
+		. ' name="submit_' . $this->for . '"'
+		. ' value="' . $this->value . '"'
+		. ' title="' . JText::_('JSEARCH_FILTER_SUBMIT') . '"'
+		. ' class="btn" style="margin-top: -10px;">'
+		. JText::_('JSEARCH_FILTER_SUBMIT')
+		. ' </button>';
+	}
+}
