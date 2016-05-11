81d4a106c09aa5f05ac92a75b0694b92a5f31ab6
Mike Brandner <mike.brandner@homepagewerkstatt.de>
2016-05-11 14:31:01 +0200

    Add files via upload

---
+++ administrator/views/edit.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * @version    CVS: 0.1.16
+ * @package    Com_Coolcineplan
+ * @author     Mike Brandner <mike@coolwebcreations.de>
+ * @copyright  Copyright (C) coolwebcreations.de 2016. All rights reserved
+ * @license    GNU General Public License Version 2 oder später; siehe LICENSE.txt
+ */
+// No direct access
+defined('_JEXEC') or die;
+
+JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
+JHtml::_('behavior.tooltip');
+JHtml::_('behavior.formvalidation');
+JHtml::_('formbehavior.chosen', 'select');
+JHtml::_('behavior.keepalive');
+
+// Import CSS
+$document = JFactory::getDocument();
+$document->addStyleSheet(JUri::root() . 'media/com_coolcineplan/css/form.css');
+?>
+<script type="text/javascript">
+	js = jQuery.noConflict();
+	js(document).ready(function () {
+		
+	js('input:hidden.show_id').each(function(){
+		var name = js(this).attr('name');
+		if(name.indexOf('show_idhidden')){
+			js('#jform_show_id option[value="'+js(this).val()+'"]').attr('selected',true);
+		}
+	});
+	js("#jform_show_id").trigger("liszt:updated");
+	});
+
+	Joomla.submitbutton = function (task) {
+		if (task == 'reservation.cancel') {
+			Joomla.submitform(task, document.getElementById('reservation-form'));
+		}
+		else {
+			
+			if (task != 'reservation.cancel' && document.formvalidator.isValid(document.id('reservation-form'))) {
+				
+				Joomla.submitform(task, document.getElementById('reservation-form'));
+			}
+			else {
+				alert('<?php echo $this->escape(JText::_('JGLOBAL_VALIDATION_FORM_FAILED')); ?>');
+			}
+		}
+	}
+</script>
+
+<form
+	action="<?php echo JRoute::_('index.php?option=com_coolcineplan&layout=edit&id=' . (int) $this->item->id); ?>"
+	method="post" enctype="multipart/form-data" name="adminForm" id="reservation-form" class="form-validate">
+
+	<div class="form-horizontal">
+		<?php echo JHtml::_('bootstrap.startTabSet', 'myTab', array('active' => 'general')); ?>
+
+		<?php echo JHtml::_('bootstrap.addTab', 'myTab', 'general', JText::_('COM_COOLCINEPLAN_TITLE_RESERVATION', true)); ?>
+		<div class="row-fluid">
+			<div class="span10 form-horizontal">
+				<fieldset class="adminform">
+
+									<input type="hidden" name="jform[id]" value="<?php echo $this->item->id; ?>" />
+				<input type="hidden" name="jform[ordering]" value="<?php echo $this->item->ordering; ?>" />
+				<input type="hidden" name="jform[state]" value="<?php echo $this->item->state; ?>" />
+				<input type="hidden" name="jform[checked_out]" value="<?php echo $this->item->checked_out; ?>" />
+				<input type="hidden" name="jform[checked_out_time]" value="<?php echo $this->item->checked_out_time; ?>" />
+
+				<?php if(empty($this->item->created_by)){ ?>
+					<input type="hidden" name="jform[created_by]" value="<?php echo JFactory::getUser()->id; ?>" />
+
+				<?php } 
+				else{ ?>
+					<input type="hidden" name="jform[created_by]" value="<?php echo $this->item->created_by; ?>" />
+
+				<?php } ?>
+				<?php if(empty($this->item->modified_by)){ ?>
+					<input type="hidden" name="jform[modified_by]" value="<?php echo JFactory::getUser()->id; ?>" />
+
+				<?php } 
+				else{ ?>
+					<input type="hidden" name="jform[modified_by]" value="<?php echo $this->item->modified_by; ?>" />
+
+				<?php } ?>				<?php echo $this->form->renderField('show_id'); ?>
+
+			<?php
+				foreach((array)$this->item->show_id as $value): 
+					if(!is_array($value)):
+						echo '<input type="hidden" class="show_id" name="jform[show_idhidden]['.$value.']" value="'.$value.'" />';
+					endif;
+				endforeach;
+			?>
+
+					<?php if ($this->state->params->get('save_history', 1)) : ?>
+					<div class="control-group">
+						<div class="control-label"><?php echo $this->form->getLabel('version_note'); ?></div>
+						<div class="controls"><?php echo $this->form->getInput('version_note'); ?></div>
+					</div>
+					<?php endif; ?>
+				</fieldset>
+			</div>
+		</div>
+		<?php echo JHtml::_('bootstrap.endTab'); ?>
+
+		<?php if (JFactory::getUser()->authorise('core.admin','coolcineplan')) : ?>
+	<?php echo JHtml::_('bootstrap.addTab', 'myTab', 'permissions', JText::_('JGLOBAL_ACTION_PERMISSIONS_LABEL', true)); ?>
+		<?php echo $this->form->getInput('rules'); ?>
+	<?php echo JHtml::_('bootstrap.endTab'); ?>
+<?php endif; ?>
+
+		<?php echo JHtml::_('bootstrap.endTabSet'); ?>
+
+		<input type="hidden" name="task" value=""/>
+		<?php echo JHtml::_('form.token'); ?>
+
+	</div>
+</form>
