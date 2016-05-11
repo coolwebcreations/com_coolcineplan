5c86d77629c1203e9b07e965be37e51e356027ee
Mike Brandner <mike.brandner@homepagewerkstatt.de>
2016-05-11 14:20:23 +0200

    Add files via upload

---
+++ administrator/sql/uninstall.mysql.utf8.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS `#__cineplan_movies`;
+DROP TABLE IF EXISTS `#__cineplan_showings`;
+DROP TABLE IF EXISTS `#__cineplan_showtypes`;
+DROP TABLE IF EXISTS `#__cineplan_auditoriums`;
+DROP TABLE IF EXISTS `#__cineplan_reservations`;
+DROP TABLE IF EXISTS `#__cineplan_users`;
+
+DELETE FROM `#__content_types` WHERE (type_alias LIKE 'com_coolcineplan.%');
