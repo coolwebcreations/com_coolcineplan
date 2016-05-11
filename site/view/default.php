731f6f328c1d1c112195bde806abb6a6b081d2f1
Mike Brandner <mike.brandner@homepagewerkstatt.de>
2016-05-11 15:08:29 +0200

    Add files via upload

---
+++ site/view/default.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * @version    CVS: 0.1.16
+ * @package    Com_Coolcineplan
+ * @author     Mike Brandner <mike@coolwebcreations.de>
+ * @copyright  Copyright (C) coolwebcreations.de 2016. All rights reserved
+ * @license    GNU General Public License Version 2 oder später; siehe LICENSE.txt
+ */
+// No direct access
+defined('_JEXEC') or die;
+
+JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
+JHtml::_('bootstrap.tooltip');
+JHtml::_('behavior.multiselect');
+JHtml::_('formbehavior.chosen', 'select');
+
+$user       = JFactory::getUser();
+$userId     = $user->get('id');
+$listOrder  = $this->state->get('list.ordering');
+$listDirn   = $this->state->get('list.direction');
+$canCreate  = $user->authorise('core.create', 'com_coolcineplan');
+$canEdit    = $user->authorise('core.edit', 'com_coolcineplan');
+$canCheckin = $user->authorise('core.manage', 'com_coolcineplan');
+$canChange  = $user->authorise('core.edit.state', 'com_coolcineplan');
+$canDelete  = $user->authorise('core.delete', 'com_coolcineplan');
+?>
+
+<form action="<?php echo JRoute::_('index.php?option=com_coolcineplan&view=auditoriums'); ?>" method="post"
+      name="adminForm" id="adminForm">
+
+	
+	<table class="table table-striped" id="auditoriumList">
+		<thead>
+		<tr>
+			<?php if (isset($this->items[0]->state)): ?>
+				<th width="5%">
+	<?php echo JHtml::_('grid.sort', 'JPUBLISHED', 'a.state', $listDirn, $listOrder); ?>
+</th>
+			<?php endif; ?>
+
+							<th class=''>
+				<?php echo JHtml::_('grid.sort',  'COM_COOLCINEPLAN_AUDITORIUMS_ID', 'a.id', $listDirn, $listOrder); ?>
+				</th>
+				<th class=''>
+				<?php echo JHtml::_('grid.sort',  'COM_COOLCINEPLAN_AUDITORIUMS_AUDITORIUM_NAME', 'a.auditorium_name', $listDirn, $listOrder); ?>
+				</th>
+				<th class=''>
+				<?php echo JHtml::_('grid.sort',  'COM_COOLCINEPLAN_AUDITORIUMS_SEATS', 'a.seats', $listDirn, $listOrder); ?>
+				</th>
+
+
+							<?php if ($canEdit || $canDelete): ?>
+					<th class="center">
+				<?php echo JText::_('COM_COOLCINEPLAN_AUDITORIUMS_ACTIONS'); ?>
+				</th>
+				<?php endif; ?>
+
+		</tr>
+		</thead>
+		<tfoot>
+		<tr>
+			<td colspan="<?php echo isset($this->items[0]) ? count(get_object_vars($this->items[0])) : 10; ?>">
+				<?php echo $this->pagination->getListFooter(); ?>
+			</td>
+		</tr>
+		</tfoot>
+		<tbody>
+		<?php foreach ($this->items as $i => $item) : ?>
+			<?php $canEdit = $user->authorise('core.edit', 'com_coolcineplan'); ?>
+
+			
+			<tr class="row<?php echo $i % 2; ?>">
+
+				<?php if (isset($this->items[0]->state)) : ?>
+					<?php $class = ($canChange) ? 'active' : 'disabled'; ?>
+					<td class="center">
+	<a class="btn btn-micro <?php echo $class; ?>" href="<?php echo ($canChange) ? JRoute::_('index.php?option=com_coolcineplan&task=auditorium.publish&id=' . $item->id . '&state=' . (($item->state + 1) % 2), false, 2) : '#'; ?>">
+	<?php if ($item->state == 1): ?>
+		<i class="icon-publish"></i>
+	<?php else: ?>
+		<i class="icon-unpublish"></i>
+	<?php endif; ?>
+	</a>
+</td>
+				<?php endif; ?>
+
+								<td>
+
+					<?php echo $item->id; ?>
+				</td>
+				<td>
+				<?php if (isset($item->checked_out) && $item->checked_out) : ?>
+					<?php echo JHtml::_('jgrid.checkedout', $i, $item->editor, $item->checked_out_time, 'auditoriums.', $canCheckin); ?>
+				<?php endif; ?>
+				<a href="<?php echo JRoute::_('index.php?option=com_coolcineplan&view=auditorium&id='.(int) $item->id); ?>">
+				<?php echo $this->escape($item->auditorium_name); ?></a>
+				</td>
+				<td>
+
+					<?php echo $item->seats; ?>
+				</td>
+
+
+								<?php if ($canEdit || $canDelete): ?>
+					<td class="center">
+					</td>
+				<?php endif; ?>
+
+			</tr>
+		<?php endforeach; ?>
+		</tbody>
+	</table>
+
+	<?php if ($canCreate) : ?>
+		<a href="<?php echo JRoute::_('index.php?option=com_coolcineplan&task=auditoriumform.edit&id=0', false, 2); ?>"
+		   class="btn btn-success btn-small"><i
+				class="icon-plus"></i>
+			<?php echo JText::_('COM_COOLCINEPLAN_ADD_ITEM'); ?></a>
+	<?php endif; ?>
+
+	<input type="hidden" name="task" value=""/>
+	<input type="hidden" name="boxchecked" value="0"/>
+	<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>"/>
+	<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>"/>
+	<?php echo JHtml::_('form.token'); ?>
+</form>
+
+<?php if($canDelete) : ?>
+<script type="text/javascript">
+
+	jQuery(document).ready(function () {
+		jQuery('.delete-button').click(deleteItem);
+	});
+
+	function deleteItem() {
+
+		if (!confirm("<?php echo JText::_('COM_COOLCINEPLAN_DELETE_MESSAGE'); ?>")) {
+			return false;
+		}
+	}
+</script>
+<?php endif; ?>
