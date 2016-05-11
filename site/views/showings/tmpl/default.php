8cebf38a83e97e414216dc7d0c66a8106e46e54f
Mike Brandner <mike.brandner@homepagewerkstatt.de>
2016-05-11 16:00:37 +0200

    Update default.php

--- site/views/showings/tmpl/default.php
+++ site/views/showings/tmpl/default.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @version    CVS: 0.1.16
- * @package    Com_Coolcineplan
+ * @package    Com_Cineplan
  * @author     Mike Brandner <mike@coolwebcreations.de>
  * @copyright  Copyright (C) coolwebcreations.de 2016. All rights reserved
  * @license    GNU General Public License Version 2 oder später; siehe LICENSE.txt
@@ -18,139 +18,95 @@ $user       = JFactory::getUser();
 $userId     = $user->get('id');
 $listOrder  = $this->state->get('list.ordering');
 $listDirn   = $this->state->get('list.direction');
-$canCreate  = $user->authorise('core.create', 'com_coolcineplan');
-$canEdit    = $user->authorise('core.edit', 'com_coolcineplan');
-$canCheckin = $user->authorise('core.manage', 'com_coolcineplan');
-$canChange  = $user->authorise('core.edit.state', 'com_coolcineplan');
-$canDelete  = $user->authorise('core.delete', 'com_coolcineplan');
+$canCreate  = $user->authorise('core.create', 'com_cineplan');
+$canEdit    = $user->authorise('core.edit', 'com_cineplan');
+$canCheckin = $user->authorise('core.manage', 'com_cineplan');
+$canChange  = $user->authorise('core.edit.state', 'com_cineplan');
+$canDelete  = $user->authorise('core.delete', 'com_cineplan');
+
+// get dates 
+$today =& JFactory::getDate('now');
+$tomorrow =& JFactory::getDate('now +1 day');
+$day3 =& JFactory::getDate('now +2 day');
+$day4 =& JFactory::getDate('now +3 day');
+$day5 =& JFactory::getDate('now +4 day');
+$day6 =& JFactory::getDate('now +5 day');
+$day7 =& JFactory::getDate('now +6 day');
+setlocale(LC_TIME, "de_DE.utf8");
 ?>
 
-<form action="<?php echo JRoute::_('index.php?option=com_coolcineplan&view=showings'); ?>" method="post"
-      name="adminForm" id="adminForm">
-
-	<?php echo JLayoutHelper::render('default_filter', array('view' => $this), dirname(__FILE__)); ?>
-	<table class="table table-striped" id="showingList">
-		<thead>
-		<tr>
-			<?php if (isset($this->items[0]->state)): ?>
-				<th width="5%">
-	<?php echo JHtml::_('grid.sort', 'JPUBLISHED', 'a.state', $listDirn, $listOrder); ?>
-</th>
-			<?php endif; ?>
-
-							<th class=''>
-				<?php echo JHtml::_('grid.sort',  'COM_COOLCINEPLAN_SHOWINGS_ID', 'a.id', $listDirn, $listOrder); ?>
-				</th>
-				<th class=''>
-				<?php echo JHtml::_('grid.sort',  'COM_COOLCINEPLAN_SHOWINGS_SHOWINGTIME', 'a.showingtime', $listDirn, $listOrder); ?>
-				</th>
-				<th class=''>
-				<?php echo JHtml::_('grid.sort',  'COM_COOLCINEPLAN_SHOWINGS_MOVIE', 'a.movie', $listDirn, $listOrder); ?>
-				</th>
-				<th class=''>
-				<?php echo JHtml::_('grid.sort',  'COM_COOLCINEPLAN_SHOWINGS_AUDITORIUM', 'a.auditorium', $listDirn, $listOrder); ?>
-				</th>
-				<th class=''>
-				<?php echo JHtml::_('grid.sort',  'COM_COOLCINEPLAN_SHOWINGS_SHOWTYPE', 'a.showtype', $listDirn, $listOrder); ?>
-				</th>
-
-
-							<?php if ($canEdit || $canDelete): ?>
-					<th class="center">
-				<?php echo JText::_('COM_COOLCINEPLAN_SHOWINGS_ACTIONS'); ?>
-				</th>
-				<?php endif; ?>
-
-		</tr>
-		</thead>
-		<tfoot>
-		<tr>
-			<td colspan="<?php echo isset($this->items[0]) ? count(get_object_vars($this->items[0])) : 10; ?>">
-				<?php echo $this->pagination->getListFooter(); ?>
-			</td>
-		</tr>
-		</tfoot>
-		<tbody>
-		<?php foreach ($this->items as $i => $item) : ?>
-			<?php $canEdit = $user->authorise('core.edit', 'com_coolcineplan'); ?>
-
+		<div ng-app="showsApp" ng-controller="showsCtrl"> 
+    
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/app.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular.min.js"></script>
+        <script src="app.js"></script>
+        
+       <div  class="container">
+			<div>
+              <?php //echo json_encode($this->items); ?>
+          	</div>
 			
-			<tr class="row<?php echo $i % 2; ?>">
-
-				<?php if (isset($this->items[0]->state)) : ?>
-					<?php $class = ($canChange) ? 'active' : 'disabled'; ?>
-					<td class="center">
-	<a class="btn btn-micro <?php echo $class; ?>" href="<?php echo ($canChange) ? JRoute::_('index.php?option=com_coolcineplan&task=showing.publish&id=' . $item->id . '&state=' . (($item->state + 1) % 2), false, 2) : '#'; ?>">
-	<?php if ($item->state == 1): ?>
-		<i class="icon-publish"></i>
-	<?php else: ?>
-		<i class="icon-unpublish"></i>
-	<?php endif; ?>
-	</a>
-</td>
-				<?php endif; ?>
-
-								<td>
-				<?php if (isset($item->checked_out) && $item->checked_out) : ?>
-					<?php echo JHtml::_('jgrid.checkedout', $i, $item->editor, $item->checked_out_time, 'showings.', $canCheckin); ?>
-				<?php endif; ?>
-				<a href="<?php echo JRoute::_('index.php?option=com_coolcineplan&view=showing&id='.(int) $item->id); ?>">
-				<?php echo $this->escape($item->id); ?></a>
-				</td>
-				<td>
-
-					<?php echo $item->showingtime; ?>
-				</td>
-				<td>
-
-					<?php echo $item->movie; ?>
-				</td>
-				<td>
-
-					<?php echo $item->auditorium; ?>
-				</td>
-				<td>
-
-					<?php echo $item->showtype; ?>
-				</td>
-
-
-								<?php if ($canEdit || $canDelete): ?>
-					<td class="center">
-					</td>
-				<?php endif; ?>
-
-			</tr>
-		<?php endforeach; ?>
-		</tbody>
-	</table>
-
-	<?php if ($canCreate) : ?>
-		<a href="<?php echo JRoute::_('index.php?option=com_coolcineplan&task=showingform.edit&id=0', false, 2); ?>"
-		   class="btn btn-success btn-small"><i
-				class="icon-plus"></i>
-			<?php echo JText::_('COM_COOLCINEPLAN_ADD_ITEM'); ?></a>
-	<?php endif; ?>
-
-	<input type="hidden" name="task" value=""/>
-	<input type="hidden" name="boxchecked" value="0"/>
-	<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>"/>
-	<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>"/>
-	<?php echo JHtml::_('form.token'); ?>
-</form>
-
-<?php if($canDelete) : ?>
-<script type="text/javascript">
-
-	jQuery(document).ready(function () {
-		jQuery('.delete-button').click(deleteItem);
-	});
-
-	function deleteItem() {
-
-		if (!confirm("<?php echo JText::_('COM_COOLCINEPLAN_DELETE_MESSAGE'); ?>")) {
-			return false;
-		}
-	}
+				<div class="container">
+					<!-- first row today + 2 days -->
+					<div class="row">
+						<div class="col-sm-4" align="center">
+							<h3><?php echo JText::_('COM_CINEPLAN_TODAY'); ?></h3>
+							<h4><?php echo '' . $today->format("l - d. F Y") . "\n"; ?></h4>
+							<-- Today Showings --></br>
+							<ul class="list-unstyled">
+								<li ng-repeat="x in shows">
+									<b>{{ x.showtime }} Uhr {{ x.movie }}</b>
+									<img class="poster" src="imgs/(x.poster)" alt="{{x.movie}}" title="{{x.showtime}} Uhr {{show.movietitle}}" width="320">
+								</li>
+							</ul>
+						</div>
+						<div class="col-sm-4" align="center">
+							<h3><?php echo JText::_('COM_CINEPLAN_TOMORROW'); ?></h3>
+							<h4><?php echo '' . $tomorrow->format("l - d. F Y") . "\n"; ?></h4>
+							<-- Tomorrow Showings --></br>
+						</div>
+						<div class="col-sm-4" align="center">
+							<h3><?php echo '' . $day3->format("l") . "\n"; ?></h3>
+							<h4><?php echo '' . $day3->format("d. F Y") . "\n"; ?></h4>
+							<-- Showings Day 3 --></br>
+						</div>
+					</div>
+					<!-- second row days 4-7 -->
+					<div class="row" align="center">
+						<div class="col-sm-3" align="center">
+							<h4><?php echo '' . $day4->format("l") . "\n"; ?></h4>
+							<p><?php echo '' . $day4->format("d. F Y") . "\n"; ?></p>
+							<-- Showings Day 4 --></br>
+						</div>
+						<div class="col-sm-3" align="center">
+							<h4><?php echo '' . $day5->format("l") . "\n"; ?></h4>
+							<p><?php echo '' . $day5->format("d. F Y") . "\n"; ?></p>
+							<-- Showings Day 5 --></br>
+						</div>
+						<div class="col-sm-3" align="center">
+							<h4><?php echo '' . $day6->format("l") . "\n"; ?></h4>
+							<p><?php echo '' . $day6->format("d. F Y") . "\n"; ?></p>
+							<-- Showings Day 6 --></br>
+						</div>
+						<div class="col-sm-3" align="center">
+							<h4><?php echo '' . $day7->format("l") . "\n"; ?></h4>
+							<p><?php echo '' . $day7->format("d. F Y") . "\n"; ?></p>
+							<-- Showings Day 7 --></br>
+						</div>
+					</div>
+					<div class="row">
+							<-- end of showings - space for other contents -->
+                    </div>
+					</div>
+		</div>
+		
+<script>
+var app = angular.module('showsApp', []);
+app.controller('showsCtrl', function($scope, $http) {
+    $http.get("index.php?option=com_cineplan&view=showingsjson&tmpl=component").then(function(response) {
+        $scope.shows = response.data.records;
+    });
+});
 </script>
-<?php endif; ?>
+          
