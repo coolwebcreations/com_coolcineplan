4b3713cc70c86178dfd4fc4cc9abd0971f401c5a
Mike Brandner <mike.brandner@homepagewerkstatt.de>
2016-05-11 15:48:36 +0200

    Add files via upload

---
+++ modules_mod_coolcineplan/assets/script.js
@@ -0,0 +1,10 @@
+/**
+ * @version     CVS: 0.1.16
+ * @package     com_coolcineplan
+ * @subpackage  mod_coolcineplan
+ * @copyright   Copyright (C) coolwebcreations.de 2016. All rights reserved
+ * @license     GNU General Public License Version 2 oder später; siehe LICENSE.txt
+ * @author      Mike Brandner <mike@coolwebcreations.de>
+ */
+
+
