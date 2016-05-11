8cf79f288dab14d79d6fc2b95019b96d86fffaf4
Mike Brandner <mike.brandner@homepagewerkstatt.de>
2016-05-11 15:58:51 +0200

    Update default.php

--- site/views/showingsjson/tmpl/default.php
+++ site/views/showingsjson/tmpl/default.php
@@ -1,1 +1,19 @@
+<?php
+/**
+ * @version    CVS: 0.1.14
+ * @package    Com_Cineplan
+ * @author     Mike Brandner <mike@coolwebcreations.de>
+ * @copyright  Copyright (C) coolwebcreations.de 2016. All rights reserved
+ * @license    GNU General Public License Version 2 oder später; siehe LICENSE.txt
+ */
+// No direct access
+defined('_JEXEC') or die;
 
+JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
+JHtml::_('bootstrap.tooltip');
+JHtml::_('behavior.multiselect');
+JHtml::_('formbehavior.chosen', 'select');
+
+?>
+
+<?php echo json_encode($this->items); ?>
