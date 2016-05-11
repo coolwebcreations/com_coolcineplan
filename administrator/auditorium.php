604b25018407c4a52163c5cdb9b3476590ca7da9
Mike Brandner <mike.brandner@homepagewerkstatt.de>
2016-05-11 14:14:05 +0200

    Add files via upload

---
+++ administrator/auditorium.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @version    CVS: 0.1.16
+ * @package    Com_Coolcineplan
+ * @author     Mike Brandner <mike@coolwebcreations.de>
+ * @copyright  Copyright (C) coolwebcreations.de 2016. All rights reserved
+ * @license    GNU General Public License Version 2 oder später; siehe LICENSE.txt
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+
+jimport('joomla.application.component.controllerform');
+
+/**
+ * Auditorium controller class.
+ *
+ * @since  1.6
+ */
+class CoolcineplanControllerAuditorium extends JControllerForm
+{
+	/**
+	 * Constructor
+	 *
+	 * @throws Exception
+	 */
+	public function __construct()
+	{
+		$this->view_list = 'auditoriums';
+		parent::__construct();
+	}
+}
