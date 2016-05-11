6441ef864b4acb2b7bf7b37159a08c086e6b0b7e
Mike Brandner <mike.brandner@homepagewerkstatt.de>
2016-05-11 14:19:27 +0200

    Add files via upload

---
+++ administrator/models/fields/filemultiple.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @version    CVS: 0.1.16
+ * @package    Com_Coolcineplan
+ * @author     Mike Brandner <mike@coolwebcreations.de>
+ * @copyright  Copyright (C) coolwebcreations.de 2016. All rights reserved
+ * @license    GNU General Public License Version 2 oder später; siehe LICENSE.txt
+ */
+
+defined('JPATH_BASE') or die;
+
+jimport('joomla.form.formfield');
+
+/**
+ * Supports an HTML select list of categories
+ *
+ * @since  1.6
+ */
+class JFormFieldFileMultiple extends JFormField
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var        string
+	 * @since    1.6
+	 */
+	protected $type = 'file';
+
+	/**
+	 * Method to get the field input markup.
+	 *
+	 * @return    string    The field input markup.
+	 *
+	 * @since    1.6
+	 */
+	protected function getInput()
+	{
+		// Initialize variables.
+		$html = '<input type="file" name="' . $this->name . '[]" multiple>';
+
+		return $html;
+	}
+}
