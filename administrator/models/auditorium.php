859f2526fbe9fc9ae69c9be885a7a8bac1f5cc4c
Mike Brandner <mike.brandner@homepagewerkstatt.de>
2016-05-13 13:56:31 +0200

    Update auditorium.php

--- administrator/models/auditorium.php
+++ administrator/models/auditorium.php
@@ -210,7 +210,7 @@ class CoolcineplanModelAuditorium extends JModelAdmin
 			if (@$table->ordering === '')
 			{
 				$db = JFactory::getDbo();
-				$db->setQuery('SELECT MAX(ordering) FROM #__cineplan_auditoriums');
+				$db->setQuery('SELECT MAX(ordering) FROM #__coolcineplan_auditoriums');
 				$max             = $db->loadResult();
 				$table->ordering = $max + 1;
 			}
