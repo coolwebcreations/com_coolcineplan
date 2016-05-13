95e33681569a6e7c8cdf6d9d34087dc6bdcf5368
Mike Brandner <mike.brandner@homepagewerkstatt.de>
2016-05-13 13:40:33 +0200

    Update uninstall.mysql.utf8.sql

--- administrator/sql/uninstall.mysql.utf8.sql
+++ administrator/sql/uninstall.mysql.utf8.sql
@@ -1,8 +1,8 @@
-DROP TABLE IF EXISTS `#__cineplan_movies`;
-DROP TABLE IF EXISTS `#__cineplan_showings`;
-DROP TABLE IF EXISTS `#__cineplan_showtypes`;
-DROP TABLE IF EXISTS `#__cineplan_auditoriums`;
-DROP TABLE IF EXISTS `#__cineplan_reservations`;
-DROP TABLE IF EXISTS `#__cineplan_users`;
+DROP TABLE IF EXISTS `#__coolcineplan_movies`;
+DROP TABLE IF EXISTS `#__coolcineplan_showings`;
+DROP TABLE IF EXISTS `#__coolcineplan_showtypes`;
+DROP TABLE IF EXISTS `#__coolcineplan_auditoriums`;
+DROP TABLE IF EXISTS `#__coolcineplan_reservations`;
+DROP TABLE IF EXISTS `#__coolcineplan_users`;
 
 DELETE FROM `#__content_types` WHERE (type_alias LIKE 'com_coolcineplan.%');
